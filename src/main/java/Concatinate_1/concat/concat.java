5b5931a6fb7cc3cf9de7ff65a45d924bac014e10
KRISHNA JANGID <krishna.jangid@publicissapient.com>
2022-02-22 17:44:16 +0530

    Concatinate-String

---
+++ src/main/java/Concatinate_1/concat/concat.java
@@ -0,0 +1,13 @@
+package Concatinate_1.concat;
+
+
+public class concat {
+
+    public static void main(String args[])
+    {
+       String str1="Krishna "; 
+        String str2=" Jangid";
+        System.out.println("Concatinated String : ");
+        System.out.println("Hello "+str1.concat(str2));
+    }
+}
